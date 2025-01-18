da0e96bc083ab647d01427e2e67799459248ebd9
qesixxx <filippovskaya.darya66603@gmail.com>
2025-01-18 14:02:40 +0300

    делаю логику

--- auth_form/myapp/views.py
+++ auth_form/myapp/views.py
@@ -1,4 +1,17 @@
+from django.contrib.auth import authenticate, login as user_login
+from django.http import HttpResponseRedirect
 from django.shortcuts import render
 
-def registration_view(request):
-    return render(request, 'myapp/registration.html')
+def login_view(request):
+    if request.method == 'POST':
+        username = request.POST.get('login')  # Используем переменную username для логина
+        password = request.POST.get('password')  # Получаем пароль из POST-запроса
+
+        usr = authenticate(request, username=username, password=password)
+        if usr is not None:
+            user_login(request, usr)
+            return HttpResponseRedirect('/')
+        else:
+            return render(request, 'myapp/login.html', {'error': 'Неверный логин или пароль'})
+
+    return render(request, 'myapp/login.html')
